be3a9dcd04a638de51bc33ef5e328975c93cc811
Partha Sarathi Bhunia <63489674+parthasarathi04@users.noreply.github.com>
2022-02-22 15:30:45 +0530

    main file

--- TheGameShow.java
+++ TheGameShow.java
@@ -1,54 +1,57 @@
 import java.util.Random;
 
 /**
- *  The Game class helps us to show the game simulation
- */
+  *  Game class helps us to show the game simulation
+  */
 class Game{
       
-	/**
+    /**
       * returns an integer based on the fact that - 
       *    the player has switched his/her door
       * 
       * @param carDoor : int --> Door with the car behind it
       * @param choiceDoor : int --> Door that the player has chosen
       * @return : int ( 1 if the player wins the car, else 0 )
+      *
       */
-	private static int swap(int carDoor, int choiceDoor){
-	    /** 
+    private static int swap(int carDoor, int choiceDoor){
+       /** 
          * If he has chosen the door with the car,
          *      switching it will make him loose the car
          * else if he hasn't chosen the door with the car,
          *      switching it will make him win the brand new car
-        */
+         */
 	    return carDoor == choiceDoor ? 0 : 1;
 	}
 	  
 	  
-	/**
+    /**
       * returns an integer based on the fact that - 
       *    the player stays with his/her chosen door
       * 
       * @param carDoor : int --> Door with the car behind it
       * @param choiceDoor : int --> Door that the player has chosen
       * @return : integer ( 1 if the player wins the car, else 0 )
+      *
       */
-	private static int stay(int carDoor, int choiceDoor){
-	    /** 
+    private static int stay(int carDoor, int choiceDoor){
+       /** 
          * If he has chosen the door with the car,
          *      staying with it will make him win the brand new car
          * else if he hasn't chosen the door with the car,
          *      staying with it will make him loose the car
-        */
+         */
 	    return carDoor == choiceDoor ? 1 : 0;
 	}
 	  
-	/**
-     * Method to start the game simulation
-     * 
-     * @param totalCase : int -> Number of times to play the game
-     * @return : void
-     */
-	public void play(int totalCase){
+    /**
+      * Method to start the game simulation
+      * 
+      * @param totalCase : int -> Number of times to play the game
+      * @return : void
+      *
+      */
+    public void play(int totalCase){
         
         Random random = new Random(); // Random object - to select random door with car and random door that the player gonna choose
         
